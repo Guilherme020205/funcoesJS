1fd1d0c4aa1e7de6fe311107a2dc0fb269e544c4
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2023-06-13 11:02:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function primeiraFuncao() {
+
+    console.log("Hello World das Funções");
+}
+
+primeiraFuncao();
+
+//
+
+function dizerNome(nome) {
+
+
+    console.log("O nome é: " + nome);
+
+}
+
+dizerNome("Guilherme");
+
+//
+
+var nomeDoBancoDeDados = "Celine";
+
+dizerNome(nomeDoBancoDeDados);
+
+//
+
+function soma(a, b) {
+
+    var soma = a + b;
+    return soma;
+
+}
+
+var somaUm = soma(2, 10);
+console.log(somaUm);
+
+var somaDois = soma(20, 10);
+console.log(somaDois);
+//ou 
+console.log(soma(200,20))
